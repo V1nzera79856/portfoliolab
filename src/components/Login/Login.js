f3ba7efd4796d4001d9287563535b76b15ec4522
V1nzera79856 <k.slaski@icloud.com>
2021-08-01 19:47:21 +0200

    login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import {WelcomeHeader} from "../WelcomeHeader/WelcomeHeader";
 
 export const Login = () => {
     return (
         <>
-            <WelcomeHeader/>
-
             <div>
                 Strona logowania
             </div>
